0609e7f6175d256b9fa742a86040c2094d46911c
Vardhman Jain <229144856+vardhman-dev@users.noreply.github.com>
2026-05-27 06:12:47 +0000

    Ask user input

--- script.js
+++ script.js
@@ -5,24 +5,46 @@ Basic Specs:
 -color cells over hovering.
 */
 
-function createGrid(){
+function createGrid() {
 
     let cellsPerRow = 16;
     let cellsPerColumn = 16;
-    let totalCells = cellsPerColumn*cellsPerRow;
+    let totalCells = cellsPerColumn * cellsPerRow;
 
-    for(let i = 0; i<totalCells; i++){
+    for (let i = 0; i < totalCells; i++) {
         let cell = document.createElement("div");
+        cell.classList.add("cell");
 
         // removed 1.6px due to left and right borders of 0.8px
-        cell.style.width = `${(600/16)-1.6}px`;
-        cell.style.height = `${(600/16)-1.6}px`;           
+        cell.style.width = `${(600 / 16) - 1.6}px`;
+        cell.style.height = `${(600 / 16) - 1.6}px`;
         cell.style.border = "1px solid black";
 
         const container = document.querySelector("#container");
         container.appendChild(cell);
     }
-
 }
 
 createGrid();
+
+function listener() {
+    let container = document.querySelector("#container");
+    container.addEventListener('mouseover', function (event) {
+        if(event.target.classList.contains('cell')){
+            event.target.style.backgroundColor = "red";
+        }
+    });
+}
+
+listener();
+
+let input = document.querySelector("#input");
+input.addEventListener("click", () => {
+    let userInput = +prompt("Enter a number between 1 and 100", 1);
+    if(userInput > 100 || userInput < 1){
+        alert("Please enter a valid input");
+    }
+    else{
+        updateGrid(userInput);
+    }
+});
